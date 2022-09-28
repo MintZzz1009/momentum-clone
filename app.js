bd7b0db798dced2ff8eeb0701e86f503bb2e70fa
Haksoo Ji <mintzzz1009@gmail.com>
2022-09-28 15:47:25 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const calcular = {
+  sum: function (numA, numB) {
+    console.log(numA + numB);
+  },
+
+  minus: function(numA, numB) {
+    console.log(numA - numB);
+  },
+
+  multiple: function(numA, numB) {
+    console.log(numA * numB);
+  },
+
+  divide: function(numA, numB) {
+    console.log(numA / numB)
+  },
+
+  powerOf: function(numA, numB) {
+    console.log(numA ** numB)
+  }
+}
