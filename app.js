03e4edda1761edd6b4c8a8247ff94b6072c31264
Haksoo Ji <mintzzz1009@gmail.com>
2022-09-29 22:28:09 +0900

    Practice addEventListener

--- app.js
+++ app.js
@@ -3,13 +3,13 @@
 document.title;
 // 'Momentum App'
 
-document.getElementById(title);
+// document.getElementById(title);
 //  null
 
 document.getElementById("title");
 // <h1 class="please" id="title">Please open your console (press F12) !!!!</h1>
 
-const newTitle = document.getElementById("title");  
+const newTitle = document.getElementById("title1");  
 newTitle.innerText = "Modifying the title of Application"; 
 
 document.getElementById("title");
@@ -21,12 +21,29 @@ document.getElementsByClassName("please");
 document.getElementsByTagName("h1");
 // HTMLCollection [h1#title.please, title: h1#title.please]
 
-console.log(title.id);
+console.log(title1.id);
 // title
 
-console.log(title.className);
+console.log(title1.className);
 // please
 
-const sayHello = document.querySelector(".hello h1");
-console.log(sayHello);
-sayHello.innerText = "Hello!!"
+const sayHello1 = document.querySelector("div.hello:first-child h1");
+console.log(sayHello1);
+sayHello1.innerText = "Hello 11"
+
+const sayHello2 = document.querySelector("div.hello:nth-child(2) h1");
+console.log(sayHello2);
+sayHello2.innerText = "Hello 22"
+sayHello2.style.color = "tomato";
+
+const sayHello3 = document.querySelector("div.hello:nth-child(3) h1");    
+// last-child 는 왜 안되는것인지?? nth-child(3)으로 하면 된다.
+console.log(sayHello3);
+sayHello3.innerText = "Please Click me!"
+
+function whenYouClick() {
+  sayHello3.style.color = "purple";
+}
+
+sayHello3.addEventListener("click",whenYouClick);
+
