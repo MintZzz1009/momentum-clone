6c5d683b2b59da1ed242e7dd69e87d2ca4b060c9
Haksoo Ji <mintzzz1009@gmail.com>
2022-10-05 20:30:46 +0900

    #5.2 PadStart

--- cloning-part/js/clock.js
+++ cloning-part/js/clock.js
@@ -9,9 +9,18 @@ setInterval(sayHello, 5000);  // 매 5초마다 sayHello 함수 실행
 setTimeout(sayHello, 5000);   // 실행하면 5초 지연 후 실행
 */
 
+// String.padStart(number, string) --> 
+// String의 길이가 number보다 짧으면 시작부분에 string을 추가해서 
+// 글자 수를 채운다.
+// "Hello".padStart(10, "ab") --> 'ababaHello'
+
+
 function getClock() {
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`
 }
 
 getClock();
