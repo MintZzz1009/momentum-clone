f154454a94fe536d98239de061330d203a896975
Haksoo Ji <mintzzz1009@gmail.com>
2022-10-05 19:59:24 +0900

    #5.1 Timeouts and Dates

--- cloning-part/js/clock.js
+++ cloning-part/js/clock.js
@@ -1,7 +1,18 @@
 const clock = document.querySelector("h2#clock");
 
+
+/*
 function sayHello() {
   console.log("Hello");
 }
+setInterval(sayHello, 5000);  // 매 5초마다 sayHello 함수 실행
+setTimeout(sayHello, 5000);   // 실행하면 5초 지연 후 실행
+*/
 
-setInterval(sayHello, 5000);
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+}
+
+getClock();
+setInterval(getClock, 1000);
