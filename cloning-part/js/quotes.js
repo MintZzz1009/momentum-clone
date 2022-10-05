a504eb8b93812eba9a26c4a3294e2e2ed98263f1
Haksoo Ji <mintzzz1009@gmail.com>
2022-10-05 23:59:09 +0900

    challenge-4

---
+++ cloning-part/js/quotes.js
@@ -0,0 +1,28 @@
+/*
+When another person makes you suffer, it is because he suffers deeply within himself, and his suffering is spilling over. He does not need punishment; he needs help. That's the message he is sending.
+-Thich Nhat Hanh
+
+To be beautiful means to be yourself. You don’t need to be accepted by others. You need to accept yourself.
+-Thich Nhat Hanh
+
+스스로를 경멸하는 사람은, 경멸하는 자신을 존중한다.
+-프리드리히 니체
+
+스스로 자신을 존경하면 다른 사람도 그대를 존경할 것이다.
+- 공자
+
+왜 자꾸 남이 하는 일만 선망하는가? 당신 자신이 되어라. 다른 사람의 자리는 모두 찼다.
+- 김난도 교수
+
+우리가 가지고 있는 15가지 재능으로 칭찬 받으려 하기보다, 가지지도 않은 한가지 재능으로 돋보이려 안달한다.
+- 마크 트웨인
+
+자신을 내보여라. 그러면 재능이 드러날 것이다.
+Put yourself on view. This brings your talents to light.
+- 발타사르 그라시안 - by. Baltasar Gracian
+
+인간은 다른 사람처럼 되고자 하기 때문에 자기 잠재력의 4분의 3을 상실한다.(아르투어 쇼펜하우어)
+
+It isn't what they call you, is's what you answer to.
+- W.C. Fields
+*/
