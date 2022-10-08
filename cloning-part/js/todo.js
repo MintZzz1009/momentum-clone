59a92601241b688a250f4aaab75726af05bd9fe6
Haksoo Ji <mintzzz1009@gmail.com>
2022-10-08 10:47:45 +0900

    Create todo-list

--- cloning-part/js/todo.js
+++ cloning-part/js/todo.js
@@ -2,28 +2,43 @@ const todoForm = document.querySelector("form#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("ul#todo-list");
 
+/*
+function editodo(event) {
+  event.target.c;
+}
+*/
 
-function handleAddNewTodo(newTodo) {
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function handleAddNewItem(newTodo) {
   const todoList_li = document.createElement("li");
-  const todoList_span = document.createElement("span");
-  todoList_li.appendChild(todoList_span);
-  todoList_span.innerText = newTodo;
+  const span = document.createElement("span");
+  const btnDelete = document.createElement("button");
+  const btnEdit = document.createElement("button");
+
+  span.innerText = newTodo;
+  btnEdit.innerText = "✏️";
+  // btnEdit.addEventListener("click", editTodo);
+ 
+  btnDelete.innerText = "❌";
+  btnDelete.addEventListener("click", deleteTodo);
+
+  todoList_li.appendChild(span);
+  todoList_li.appendChild(btnEdit);
+  todoList_li.appendChild(btnDelete);
   todoList.appendChild(todoList_li);
 }
 
-
-function handleAddTodoList(event) {
+function handleAddNewTodoList(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = "";
-  console.log(newTodo);
 
-  handleAddNewTodo(newTodo);
-  
+  handleAddNewItem(newTodo);
 }
 
 
-
-
-
-todoForm.addEventListener("submit", handleAddTodoList);
+todoForm.addEventListener("submit", handleAddNewTodoList);
