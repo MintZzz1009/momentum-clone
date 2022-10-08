fd499f37ffa753a6a6fea5d48096978730b18023
Haksoo Ji <mintzzz1009@gmail.com>
2022-10-08 11:59:42 +0900

    Update todo.js

--- cloning-part/js/todo.js
+++ cloning-part/js/todo.js
@@ -2,11 +2,30 @@ const todoForm = document.querySelector("form#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("ul#todo-list");
 
-/*
-function editodo(event) {
-  event.target.c;
+function editTodo(event) {
+  const span = event.target.parentElement.querySelector("span");
+  span.classlist.add("hidden");
+  
+  const formToEdit = document.createElement("form");
+  const input = formToEdit.createElement("input");
+  const originText = span.text();
+  input.append(originText);
+  // input.innerText = originText;
+
+  const btnEdit = formToEdit.createElement("button");
+  btnEdit.innerText = "✅";
+  btnEdit.addEventListener("submit", submitNewTodo);
+
+  span.parentElement.appendChild(formToEdit)
+
+  function submitNewTodo() {
+    event.preventDefault();
+    const newText = input.value
+    formToEdit.remove()
+    span.innerText = newText;
+    span.classList.remove("hidden");
+  }
 }
-*/
 
 function deleteTodo(event) {
   const li = event.target.parentElement;
@@ -21,7 +40,7 @@ function handleAddNewItem(newTodo) {
 
   span.innerText = newTodo;
   btnEdit.innerText = "✏️";
-  // btnEdit.addEventListener("click", editTodo);
+  btnEdit.addEventListener("click", editTodo);
  
   btnDelete.innerText = "❌";
   btnDelete.addEventListener("click", deleteTodo);
