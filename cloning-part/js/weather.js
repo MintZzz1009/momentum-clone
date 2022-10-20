3dd1c95594851015f990bfef0bb02c31a1f37742
Haksoo Ji <mintzzz1009@gmail.com>
2022-10-20 23:54:29 +0900

    #8.1 Weather API end

--- cloning-part/js/weather.js
+++ cloning-part/js/weather.js
@@ -4,8 +4,14 @@ function onGeoOK(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  fetch(url).then(response => response.json()).then(data => console.log(data.name, data.weather[0].main));
-
+  fetch(url)
+  .then(response => response.json())
+  .then(data => { 
+  const cityName = document.querySelector("#weather span:first-child");
+  const currentWeather = document.querySelector("#weather span:last-child")
+  cityName.innerText = data.name;
+  currentWeather.innerText =data.weather[0].main;
+  })
 }
 
 function onGeoError() {
