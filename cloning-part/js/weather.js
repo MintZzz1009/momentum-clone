112e141e5319323395720ab522f91763b3ae65f1
Haksoo Ji <mintzzz1009@gmail.com>
2022-10-20 22:51:07 +0900

    #8.0 Geolocation

--- cloning-part/js/weather.js
+++ cloning-part/js/weather.js
@@ -1,11 +1,15 @@
 function onGeoOK(position) {
-  const lat = positon.coords.latitude;
-  const lng = positon.coords.longitude;
-  console.log("You live in", lat, lng);
+
+
+  console.log(position);
+  
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  console.log("You live it", lat, lng);
 }
 
 function onGeoError() {
   alert("Can't find you. No weather for you.")
 }
 
-navigator.geolocation.getCurrentPosition
+navigator.geolocation.getCurrentPosition(onGeoOK,onGeoError);
